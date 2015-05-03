58d2b750d16153685de2c43d3e86327fee83bbb9
Alex Ung <alexung.ucsb@gmail.com>
2015-05-02 18:03:14 -0700

    Took html for the layout, and for the index and creating a user

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET /users listing. */
+// Routes here default to begin with /users
+// GET /users listing.
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* GET /users/view */
-router.get('/view', function(req, res, next) {
-  res.send('user view');
+// GET /users/create
+router.get('/create', function(req, res, next) {
+  // vm stands for view model, can put it here instead of in the res.render()
+  var vm = {
+    title: "Create an account"
+  }
+  res.render('users/create', vm);
 });
 
 module.exports = router;
