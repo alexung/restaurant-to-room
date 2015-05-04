935e5c77435fffeb2b7e5425afc9c7f79c88405e
Alex Ung <alexung.ucsb@gmail.com>
2015-05-03 23:26:21 -0700

    add the user.js model and, within app.js, require var mongoose, var config, and mongoose.connect(config.mongoUri)

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 // routes that you can access once you npm start
+var config = require('./config');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var orders = require('./routes/orders');
+
+mongoose.connect(config.mongoUri);
 
 var app = express();
 
@@ -26,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // base path for each set of routes, can access these via npm start
 app.use('/', routes);
 app.use('/users', users);
+app.use('/orders', orders);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var userService = require('../services/user-service');
 // Routes here default to begin with /users
 // GET /users listing.
 router.get('/', function(req, res, next) {
@@ -12,8 +12,24 @@ router.get('/create', function(req, res, next) {
   // vm stands for view model, can put it here instead of in the res.render()
   var vm = {
     title: "Create an account"
-  }
+  };
   res.render('users/create', vm);
+});
+
+// POST /users/create
+router.post('/create', function(req, res, next) {
+  userService.addUser(req.body, function(err) {
+  if (err) {
+    var vm = {
+      title: "Create an account",
+      input: req.body,
+      error: 'Something went wrong'
+    };
+    delete vm.input.password;
+    return res.render('users/create', vm);
+  }
+  res.redirect('/orders');
+  });
 });
 
 module.exports = router;
