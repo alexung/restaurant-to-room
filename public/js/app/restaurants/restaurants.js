b4e165b20aede1f9271c8d6973faaab70eefbeb5
Alex Ung <alexung.ucsb@gmail.com>
2015-05-15 13:09:24 -0700

    separated what was initially orders/index.hbs into restaurants.html but left scripts in index.hbs, it appears on the login page

--- public/js/app/restaurants/restaurants.js
+++ public/js/app/restaurants/restaurants.js
@@ -1,1 +1,12 @@
-restaurants.js
+(function(){
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('RestaurantsController', RestaurantsController);
+
+    function RestaurantsController() {
+      this.data = 'the data'; //use {{ vm.data }} in the view
+    }
+}());
+// vm is viewmodel - glue between controller and view
